1c617774271d043b62b006479b745f0e3fc0f500
PProvost <peter@provost.org>
2009-04-24 14:13:23 -0600

    Code cleanup and better messaging for unit names that aren't available

--- Lazviter.lua
+++ Lazviter.lua
@@ -18,11 +18,7 @@ limitations under the License.
 
 local addonName = "La-Z-Viter"
 
--- Locals
-local L = setmetatable({}, {__index=function(t,i) return i end})
-local defaults, defaultsPC, db, dbpc = {}, {}
-
--- Utility function
+-- Utility functions
 local function Print(...) print("|cFF33FF99" .. addonName .. "|r:", ...) end
 local debugf = tekDebug and tekDebug:GetFrame("Lazviter")
 local function Debug(...) if debugf then debugf:AddMessage(string.join(", ", tostringall(...))) end end
@@ -34,37 +30,9 @@ Lazviter:RegisterEvent("ADDON_LOADED")
 
 function Lazviter:ADDON_LOADED(event, addon)
 	if addon:lower() ~= addonName:lower() then return end
-
-	LazviterDB, LazviterDBPC = setmetatable(LazviterDB or {}, {__index = defaults}), setmetatable(LazviterDBPC or {}, {__index = defaultsPC})
-	db, dbpc = LazviterDB, LazviterDBPC
-
-	-- Do anything you need to do after addon has loaded
-
 	LibStub("tekKonfig-AboutPanel").new(nil, "Lazviter") -- Make first arg nil if no parent config panel
-
 	self:UnregisterEvent("ADDON_LOADED")
 	self.ADDON_LOADED = nil
-
-	if IsLoggedIn() then self:PLAYER_LOGIN() else self:RegisterEvent("PLAYER_LOGIN") end
-end
-
-function Lazviter:PLAYER_LOGIN()
-	self:RegisterEvent("PLAYER_LOGOUT")
-
-	-- Do anything you need to do after the player has entered the world
-
-	self:UnregisterEvent("PLAYER_LOGIN")
-	self.PLAYER_LOGIN = nil
-end
-
-function Lazviter:PLAYER_LOGOUT()
-	for i,v in pairs(defaults) do if db[i] == v then db[i] = nil end end
-	for i,v in pairs(defaultsPC) do if dbpc[i] == v then dbpc[i] = nil end end
-
-	-- Do anything you need to do as the player logs out
-end
-
-local function invite(name)
 end
 
 function Lazviter:DoInvites(approved, standby)
@@ -83,8 +51,12 @@ function Lazviter:ProcessApprovedList()
 			break
 		else
 			local name = table.remove(approved)
-			if not (UnitInRaid(name) or UnitInParty(name)) then
+			if not UnitExists(name) then
+				Print("Unit does not exist: " .. name)
+			elseif not (UnitInRaid(name) or UnitInParty(name)) then
 				InviteUnit(name)
+			else
+				Print("Unable to invite " .. name)
 			end
 		end
 	end
